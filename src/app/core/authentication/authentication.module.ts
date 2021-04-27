594a38f805f56e9892695dc78ca4b192a11ad3b2
Daniel Douglas <dan.douglas.01@gmail.com>
2021-04-27 13:05:16 +0100

    task(core): add core and auth module

---
+++ src/app/core/authentication/authentication.module.ts
@@ -0,0 +1,15 @@
+/*
+* Any authentication components/services etc. will live in this module
+*/
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthenticationModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('The core module has already been loaded. You should only import core modules once into the app module.');
+    }
+  }
+}
