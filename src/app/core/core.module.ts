1aff64c0b4f06d5a5a0031742fda5e44ac81566b
Daniel Douglas <dan.douglas.01@gmail.com>
2021-04-27 15:07:56 +0100

    task(nav): add module and component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 
 
 @NgModule({
   declarations: [],
-  imports: [
+  exports: [
     CommonModule,
     AuthenticationModule,
+    NavigationModule,
   ]
 })
 export class CoreModule {
